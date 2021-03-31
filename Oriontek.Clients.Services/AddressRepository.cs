76bf7da9727422c2e5fc3032037fd49362cf51ad
Jarlyn Polanco <jarlynpolanco@gmail.com>
2021-03-30 20:34:58 -0400

    Creating repositorys

---
+++ Oriontek.Clients.Services/AddressRepository.cs
@@ -0,0 +1,59 @@
+﻿using Oriontek.Clients.Contracts;
+using Oriontek.Clients.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Oriontek.Clients.Data.Models;
+using System.Threading.Tasks;
+
+namespace Oriontek.Clients.Services
+{
+    public class AddressRepository : IAddressRepository
+    {
+
+        private readonly AppDbContext _dbContext;
+
+        public AddressRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Create(Address entity)
+        {
+            await _dbContext.Addresses.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Address entity)
+        {
+            _dbContext.Addresses.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Address>> FindAll()
+        {
+            var address = await _dbContext.Addresses.Include(x => x.Client).ToListAsync();
+            return address;
+        }
+
+        public async Task<Address> FindById(int id)
+        {
+            var address = await _dbContext.Addresses.Include(x => x.Client)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return address;
+        }
+
+        public async Task<bool> Update(Address entity)
+        {
+            _dbContext.Addresses.Update(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _dbContext.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> IsExist(int id) => await _dbContext.Addresses.AnyAsync(x => x.Id == id);
+    }
+}
---
+++ Oriontek.Clients.Services/ClientRepository.cs
@@ -0,0 +1,58 @@
+﻿using Oriontek.Clients.Contract;
+using Oriontek.Clients.Data;
+using Oriontek.Clients.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Oriontek.Clients.Services
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ClientRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Create(Client entity)
+        {
+            await _dbContext.Clients.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Client entity)
+        {
+            _dbContext.Clients.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Client>> FindAll()
+        {
+            var clients = await _dbContext.Clients.Include(x => x.Addresses).ToListAsync();
+            return clients;
+        }
+
+        public async Task<Client> FindById(int id)
+        {
+            var client = await _dbContext.Clients.Include(x => x.Addresses)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return client;
+        }
+
+        public async Task<bool> Update(Client entity)
+        {
+            _dbContext.Clients.Update(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _dbContext.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> IsExist(int id) => await _dbContext.Clients.AnyAsync(x => x.Id == id);
+    }
+}
