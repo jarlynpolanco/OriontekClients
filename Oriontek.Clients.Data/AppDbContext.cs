52417983d7ef0b5b99dde311f179205c7ae5abab
Jarlyn Polanco <jarlynpolanco@gmail.com>
2021-03-30 20:33:44 -0400

    Creating database context

---
+++ Oriontek.Clients.Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Oriontek.Clients.Data.Models;
+
+namespace Oriontek.Clients.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Address>()
+               .HasOne(a => a.Client)
+               .WithMany(b => b.Addresses)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
