59217c11640f87a932f9c177ae9fd2e28e5fa64c
Jarlyn Polanco <jarlynpolanco@gmail.com>
2021-03-30 20:33:13 -0400

    Creating models

---
+++ Oriontek.Clients.Data/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oriontek.Clients.Data.Models
+{
+    [Table("Addresses")]
+    public class Address
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Street { get; set; }
+
+        public string Section { get; set; }
+
+        public string Province { get; set; }
+
+        public string Municipality { get; set; }
+
+        public string District { get; set; }
+
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; }
+
+    }
+}
---
+++ Oriontek.Clients.Data/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oriontek.Clients.Data.Models
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string DocumentNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public virtual IList<Address> Addresses { get; set; }
+    }
+}
